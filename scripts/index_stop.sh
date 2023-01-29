190d1f386edb0d9cecbc7234434548d8608ff5b3
ivanklimuk <ivanklimuk96@gmail.com>
2023-01-30 01:28:28 +0200

    Fix stop script

--- scripts/index_stop.sh
+++ scripts/index_stop.sh
@@ -9,9 +9,9 @@ REDIS_HOST=$2
 REDIS_PORT=$3
 REDIS_PASSWORD=$4
 
-services=$(ssh root@$SWARM_MANAGER "docker service ls | grep index_gen_ | awk '{print $2}'")
-for service in $services; do
+services=$(ssh root@${SWARM_MANAGER} "docker service ls --format '{{.Name}}' | grep '^index_gen_'")
+for service_name in $services; do
   echo "Stop and remove service $service"
-  ssh root@$SWARM_MANAGER "redis-cli -h $REDIS_HOST -p $REDIS_PORT -a $REDIS_PASSWORD del $service"
+  ssh root@$SWARM_MANAGER "redis-cli -h $REDIS_HOST -p $REDIS_PORT -a $REDIS_PASSWORD del $service_name"
   ssh root@$SWARM_MANAGER "docker service rm $service_name"
 done
